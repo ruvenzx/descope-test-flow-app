b0e9f8ef2e4de8b433bfb8243bdebe91a2031d78
Reuven Zabirov <ruvenzx0@gmail.com>
2024-03-18 16:05:13 +0200

    auth layer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 
 import Container from './containers';
 import './App.css';
-import { AuthProvider } from '@descope/react-sdk';
+import { AuthProvider, Descope, useSession } from '@descope/react-sdk';
 
 const AppRoot = () => {
   return (
-    <AuthProvider projectId="P2c0GgXKv4F3i4UnSViLiyqVzAVT" flowId="sign-in"> 
+    <AuthProvider projectId="P2c0GgXKv4F3i4UnSViLiyqVzAVT"> 
     <App/>
     </AuthProvider>
   );
 }
 
 const App = () => {
-  return (
-    <div className="App">
-      <Container />
-    </div>
-  );
+  const {isAuthenticated, isSessionLoading} = useSession();
+
+
+	if (isSessionLoading) {
+		return <p>Loading...</p>;
+	}
+
+  if (isAuthenticated) {
+    return (
+      <div className="App">
+        <Container />
+      </div>
+    );
+	}
+
+	return <p>You are not logged in, please do:
+    <Descope flowId='sign-in'></Descope>
+  </p>;
 };
 
 export default AppRoot;
